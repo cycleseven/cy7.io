a00efc7e358866cb948ab65855253b2f5873a95d
cycleseven <unvexer@gmail.com>
2019-10-19 20:49:35 +0100

    Fix linting issues. Add prettier --check to full lint task

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,26 +1,9 @@
+/* eslint-disable global-require */
+
 import React from "react";
 import { addDecorator, configure } from "@storybook/react";
 
 import { Cy7ThemeProvider } from "../src/designPrimitives";
-
-/**
- * Gatsby's Link overrides:
- *
- * Gatsby defines a global called ___loader. It's mocked out to prevent its
- * method calls from creating console errors.
- */
-// global.___loader = {
-//   enqueue: () => {},
-//   hovering: () => {}
-// };
-
-// Gatsby internal mocking to prevent unnecessary errors in storybook testing environment
-global.__PATH_PREFIX__ = "";
-
-// This is to utilized to override the window.___navigate method Gatsby defines and uses to report what path a Link would be taking us to if it wasn't inside a storybook
-window.___navigate = pathname => {
-  action("Navigate to:")(pathname);
-};
 
 /**
  * Wrap all stories with cy7 default styles.
