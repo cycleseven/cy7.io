95ead2d30d23e8fe0bbe73f6951e6c2c639910e8
cycleseven <unvexer@gmail.com>
2019-12-29 21:03:15 +0000

    Use custom favicon size config to work around jagged favicon in Firefox
    See https://github.com/gatsbyjs/gatsby/issues/19899 (hopefully will lead to a long-term fix)

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,28 @@ module.exports = {
         background_color: "#000000",
         theme_color: "#000000",
         display: "minimal-ui",
-        icon: "src/images/favicon.png"
+        icon: "src/images/favicon.png",
+        icons: [
+          // 32x32 for desktop browser favicon
+          // https://github.com/gatsbyjs/gatsby/issues/19899#issuecomment-562995028
+          {
+            src: "/favicons/icon-32x32.png",
+            sizes: "32x32",
+            type: "image/png"
+          },
+          // 192 and 512 for web app manifest
+          // https://developers.google.com/web/fundamentals/web-app-manifest
+          {
+            src: "/favicons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/favicons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
       }
     },
 
