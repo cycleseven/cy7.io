2d073db1b16f6f5e0ab3914858ad98568e659762
cycleseven <unvexer@gmail.com>
2020-01-04 20:20:33 +0000

    Update dependencies

--- src/@cy7/designSystem/Cy7ThemeProvider.js
+++ src/@cy7/designSystem/Cy7ThemeProvider.js
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import * as theme from "./theme";
-import { GlobalStyles } from "./globalStyles";
+import { GlobalStyles } from "./GlobalStyles";
 
 function Cy7ThemeProvider({ children, ...props }) {
   return (
