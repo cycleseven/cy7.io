5527bca99f785619fc7a2a2bfe69220135058dee
cycleseven <unvexer@gmail.com>
2020-12-27 12:20:13 +0000

    Move @font-face declarations outside of Styled Components to avoid re-fetching

--- src/@cy7/designSystem/Cy7ThemeProvider.js
+++ src/@cy7/designSystem/Cy7ThemeProvider.js
@@ -2,13 +2,11 @@ import PropTypes from "prop-types";
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import theme from "./theme";
-import { Fonts } from "./Fonts";
 import { GlobalStyles } from "./GlobalStyles";
 
 function Cy7ThemeProvider({ children, ...props }) {
   return (
     <ThemeProvider theme={theme} {...props}>
-      <Fonts />
       <GlobalStyles />
       {children}
     </ThemeProvider>
--- src/@cy7/gatsby/Page.js
+++ src/@cy7/gatsby/Page.js
@@ -6,6 +6,22 @@ import PropTypes from "prop-types";
 import React from "react";
 import styled from "styled-components";
 
+// I'd ideally prefer to define fonts inside of a createGlobalStyle, via
+// Cy7ThemeProvider. That way the entire design system can be set up with a
+// single import.
+//
+// But using @font-face inside of Styled Components causes fonts to be
+// re-fetched on each re-render, horrible for performance and UX. So it's worth
+// using regular global CSS for the fonts.
+//
+// It would also be possible to have this side-effect import in Cy7ThemeProvider,
+// but that could impact ability to tree-shake the design system.
+//
+// See:
+// https://github.com/styled-components/styled-components/issues/2227
+// https://github.com/styled-components/styled-components/issues/2900
+import "@cy7/designSystem/fonts.css";
+
 const MainContent = styled.div`
   flex-grow: 1;
 `;
