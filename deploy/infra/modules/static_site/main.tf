c7f6f052ab0ae3537dc98caf02e6cdc9d3ad8615
cycleseven <unvexer@gmail.com>
2021-02-28 14:57:54 +0000

    Upgrade TLS version used by Cloudfront

--- deploy/infra/modules/static_site/main.tf
+++ deploy/infra/modules/static_site/main.tf
@@ -115,8 +115,9 @@ resource "aws_cloudfront_distribution" "web" {
   }
 
   viewer_certificate {
-    acm_certificate_arn = data.aws_acm_certificate.root.arn
-    ssl_support_method  = "sni-only"
+    acm_certificate_arn      = data.aws_acm_certificate.root.arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2019"
   }
 
   custom_error_response {
