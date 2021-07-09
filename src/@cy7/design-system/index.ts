af0bedc5357a9bc7dc3d2af378c6de99741d32e5
cycleseven <unvexer@gmail.com>
2021-07-09 17:27:36 +0100

    Rename "primitives" directory to "components"

--- src/@cy7/design-system/index.ts
+++ src/@cy7/design-system/index.ts
@@ -1,15 +1,15 @@
 export * from "@cy7/stitches";
 
-// Primitives
-export { default as Link } from "@cy7/design-system/primitives/Link";
-export { default as Svg } from "@cy7/design-system/primitives/Svg";
-
-// Primitive types
-import { SvgProps } from "@cy7/design-system/primitives/Svg";
-export type { SvgProps };
+// Components
+export { default as Link } from "@cy7/design-system/components/Link";
+export { default as Svg } from "@cy7/design-system/components/Svg";
 
 // Layout
 export { default as Grid } from "@cy7/design-system/layout/Grid";
 export { default as GutterBox } from "@cy7/design-system/layout/GutterBox";
 export { default as MaxWidth } from "@cy7/design-system/layout/MaxWidth";
 export { default as Stack } from "@cy7/design-system/layout/Stack";
+
+// Types
+import { SvgProps } from "@cy7/design-system/components/Svg";
+export type { SvgProps };
