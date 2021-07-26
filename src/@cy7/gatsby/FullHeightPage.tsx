d14e3b171cb2e91caa431501de910dc8acc745d2
cycleseven <unvexer@gmail.com>
2021-07-26 21:14:18 +0100

    Migrate <FullHeightPage /> to Stitches

---
+++ src/@cy7/gatsby/FullHeightPage.tsx
@@ -0,0 +1,47 @@
+import React, { ComponentProps, useLayoutEffect } from "react";
+import { global, styled } from "@cy7/design-system";
+
+const FullHeightContainer = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+  flexGrow: 1,
+  minWidth: "300px",
+});
+
+const applyGlobalStyles = global({
+  "html.cy7-full-height-page": {
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "100%",
+  },
+
+  "html.cy7-full-height-page body, html.cy7-full-height-page #___gatsby, html.cy7-full-height-page #gatsby-focus-wrapper": {
+    display: "flex",
+    flexDirection: "column",
+    flexGrow: 1,
+  }
+});
+
+type FullHeightPageProps = ComponentProps<typeof FullHeightContainer>;
+
+function FullHeightPage(props: FullHeightPageProps): JSX.Element {
+  useLayoutEffect(() => {
+    applyGlobalStyles();
+  }, []);
+
+  useLayoutEffect(() => {
+    document.documentElement.classList.add("cy7-full-height-page");
+
+    return function onFullHeightPageUnmount() {
+      document.documentElement.classList.remove("cy7-full-height-page");
+    };
+  }, []);
+
+  return (
+    <React.Fragment>
+      <FullHeightContainer {...props} />
+    </React.Fragment>
+  );
+}
+
+export { FullHeightPage };
