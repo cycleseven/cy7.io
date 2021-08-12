22960d9e3261a763518612ea45396d0f72f88e99
cycleseven <unvexer@gmail.com>
2021-08-12 17:00:48 +0100

    Delete legacy design system 😢👋

--- src/@cy7/designSystem/index.d.ts
+++
@@ -1,4 +0,0 @@
-// TODO: this is bad, but @cy7/designSystem is going to be deprecated when migration to Typescript + Stitches
-// is complete. In the interim, this hides type related errors from this package so they don't drown out
-// real type errors in the noise.
-declare module "@cy7/designSystem";
--- src/@cy7/designSystem/primitives/scales.js
+++
@@ -1,7 +0,0 @@
-const baseBorderRadiusPx = 2;
-
-function borderRadius(multiplier = 1) {
-  return `${multiplier * baseBorderRadiusPx}px`;
-}
-
-export { borderRadius };
