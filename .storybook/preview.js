bd354a1d0f20e587c4068fdc7e2146e451857489
cycleseven <unvexer@gmail.com>
2021-09-08 00:09:44 +0100

    Add intro guides to Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -11,40 +11,47 @@ addDecorator((storyFn) => storyFn());
  * Note: the return value defines the ordering of the stories.
  */
 function loadStories() {
-  const introStory = require("../src/@cy7/docs/overview/Intro.stories.mdx");
-  const techStackStory = require("../src/@cy7/docs/overview/TechStack.stories.mdx");
-  const designSystemStory = require("../src/@cy7/docs/overview/DesignSystem.stories.mdx");
-  const designSystemSection = require.context(
+  //  Intro
+  const helloWorldStory = require("../src/@cy7/docs/intro/HelloWorld.stories.mdx");
+  const projectLayoutStory = require("../src/@cy7/docs/intro/ProjectLayout.stories.mdx");
+
+  // Guides, in order
+  const bloggingStory = require("../src/@cy7/docs/guides/Blogging.stories.mdx");
+  const codeQualityStory = require("../src/@cy7/docs/guides/CodeQuality.stories.mdx");
+  const accessibilityStory = require("../src/@cy7/docs/guides/Accessibility.stories.mdx");
+  const typescriptStory = require("../src/@cy7/docs/guides/TypeScript.stories.mdx");
+  const testingStory = require("../src/@cy7/docs/guides/Testing.stories.mdx");
+  const deploymentStory = require("../src/@cy7/docs/guides/Deployment.stories.mdx");
+
+  // Design system docs
+  const designSystemIntroStory = require("../src/@cy7/docs/design-system/GettingStarted.stories.mdx");
+  const designSystemComponents = require.context(
     "../src/@cy7/docs/design-system",
     true,
     /\.stories\.(mdx|js)$/
   );
-  const componentsSection = require.context(
-    "../src/@cy7/docs/components",
-    true,
-    /\.stories\.(mdx|js)$/
-  );
-  const gatsbySection = require.context(
-    "../src/@cy7/docs/gatsby",
-    true,
-    /\.stories\.(mdx|js)$/
-  );
-  const otherStories = require.context(
-    "../src/@cy7/docs",
+
+  // Other
+  const websiteSection = require.context(
+    "../src/@cy7/docs/website",
     true,
     /\.stories\.(mdx|js)$/
   );
 
   return [
-    introStory,
-    techStackStory,
-    designSystemStory,
-    ...designSystemSection
+    helloWorldStory,
+    projectLayoutStory,
+    bloggingStory,
+    codeQualityStory,
+    accessibilityStory,
+    typescriptStory,
+    testingStory,
+    deploymentStory,
+    designSystemIntroStory,
+    ...designSystemComponents
       .keys()
-      .map((filename) => designSystemSection(filename)),
-    ...componentsSection.keys().map((filename) => componentsSection(filename)),
-    ...gatsbySection.keys().map((filename) => gatsbySection(filename)),
-    ...otherStories.keys().map((filename) => otherStories(filename)),
+      .map((filename) => designSystemComponents(filename)),
+    ...websiteSection.keys().map((filename) => websiteSection(filename)),
   ];
 }
 
