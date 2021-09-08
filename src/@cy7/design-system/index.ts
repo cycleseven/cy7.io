758611633e33e33a6553c09526398718e8f5cc46
cycleseven <unvexer@gmail.com>
2021-09-08 15:57:08 +0100

    Treat <Box /> as a layout component

--- src/@cy7/design-system/index.ts
+++ src/@cy7/design-system/index.ts
@@ -7,12 +7,12 @@ export { applyGlobalStyles } from "@cy7/design-system/GlobalStyles";
 
 // Components
 export { default as LinkIcon } from "@cy7/design-system/icons/LinkIcon";
-export { default as Box } from "@cy7/design-system/components/Box";
 export { default as Link } from "@cy7/design-system/components/Link";
 export { default as Logo } from "@cy7/design-system/components/Logo";
 export { default as Svg } from "@cy7/design-system/components/Svg";
 
 // Layout
+export { default as Box } from "@cy7/design-system/layout/Box";
 export { default as Grid } from "@cy7/design-system/layout/Grid";
 export { default as GutterBox } from "@cy7/design-system/layout/GutterBox";
 export { default as MaxWidth } from "@cy7/design-system/layout/MaxWidth";
