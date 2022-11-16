3ba974aff9a5c791e051a3490deaa36f215157a2
Owen Gillespie <owen@cy7.io>
2022-11-16 20:33:45 +0000

    Switch from yarn to pnpm

--- README.md
+++ README.md
@@ -7,10 +7,10 @@ Source code for [cy7.io](https://cy7.io), built with Gatsby.
 System requirements for running this project locally are:
 
 - Node.js v16
-- Yarn v1
+- pnpm v7
 - Terraform v1
 
-1. **Check the `engines` field of `package.json` for the required versions of Node and Yarn.** Make sure you have those installed.
+1. **Check the `engines` field of `package.json` for the required versions of `node` and `pnpm`.** Make sure you have those installed.
 
    You can check with the following commands:
 
@@ -18,26 +18,26 @@ System requirements for running this project locally are:
    $ node --version
    v16.18.0
 
-   $ yarn --version
-   1.22.19
+   $ pnpm --version
+   7.16.1
    ```
 
 2. **Install dependencies.**
 
    ```bash
-   yarn
+   pnpm install
    ```
 
 3. **That's it!** Happy developing. You can start an instance of Gatsby's [development server](https://www.gatsbyjs.com/docs/tutorial/part-1/#run-your-site-locally) like this:
 
    ```bash
-   yarn gatsby develop
+   pnpm exec gatsby develop
    ```
 
    Or equivalently:
 
    ```bash
-   yarn start
+   pnpm start
    ```
 
    Once that's running, navigate to [localhost:8000](http://localhost:8000/) in your browser of choice to view the site.
@@ -52,20 +52,20 @@ That means that for now, in practice, Terraform is also a requirement for develo
 
 ## Git hooks
 
-To improve code quality, automated checks take place when committing and pushing code via `git commit` and `git push`, using [Git hooks](https://www.atlassian.com/git/tutorials/git-hooks). These are set up for you behind-the-scenes when installing dependencies with `yarn` by [Lefthook](https://github.com/evilmartians/lefthook).
+To improve code quality, automated checks take place when committing and pushing code via `git commit` and `git push`, using [Git hooks](https://www.atlassian.com/git/tutorials/git-hooks). These are set up for you behind-the-scenes when installing dependencies with `pnpm` by [Lefthook](https://github.com/evilmartians/lefthook).
 
 You can read more about this project's code quality guidance in the [developer guide](https://storybook.cy7.io/?path=/docs/guides-code-quality--page).
 
 ## Other useful commands
 
-- **Build a production bundle:** `yarn gatsby build` (or just `yarn build`)
-- **Serve that built production bundle locally:** `yarn gatsby serve`
-- **Run Jest test suite:** `yarn jest` (or `yarn test`)
-- **Check for ESLint violations:** `yarn eslint src`
-- **Attempt to fix lint violations automatically:** `yarn eslint --fix src`
-- **Check for TypeScript errors:** `yarn tsc --noEmit`
-- **Check for Prettier violations:** `yarn prettier --check .`
-- **Format all code with Prettier:** `yarn prettier --write .`
-- **Run all code quality checks at once:** `yarn lefthook run check-all`
-- **Run Storybook:** `yarn storybook`
-- **Build Storybook:** `yarn build-storybook --docs`
+- **Build a production bundle:** `pnpm exec gatsby build` (or just `pnpm run build`)
+- **Serve that built production bundle locally:** `pnpm exec gatsby serve`
+- **Run Jest test suite:** `pnpm exec jest` (or `pnpm test`)
+- **Check for ESLint violations:** `pnpm exec eslint src`
+- **Attempt to fix lint violations automatically:** `pnpm exec eslint --fix src`
+- **Check for TypeScript errors:** `pnpm exec tsc --noEmit`
+- **Check for Prettier violations:** `pnpm exec prettier --check .`
+- **Format all code with Prettier:** `pnpm exec prettier --write .`
+- **Run all code quality checks at once:** `pnpm exec lefthook run check-all`
+- **Run Storybook:** `pnpm run storybook`
+- **Build Storybook:** `pnpm exec build-storybook --docs`
--- src/@cy7/design-system/stitches.config.ts
+++ src/@cy7/design-system/stitches.config.ts
@@ -9,125 +9,118 @@ import { space } from "@cy7/design-system/theme/space";
 import typography from "@cy7/typography";
 import { VariantProps, createStitches, PropertyValue } from "@stitches/react";
 
-const {
-  styled,
-  createTheme,
-  css,
-  globalCss,
-  keyframes,
-  getCssText,
-  theme,
-} = createStitches({
-  theme: {
-    radii: {
-      "2": "2px",
-    },
-    borderWidths: {
-      linkFocus: "3px",
-    },
-    colors: {
-      // Base tones
-      angrypeach: `hsl(${angrypeach}, 100%, 80%)`,
-      candyfloss: `hsl(${candyfloss}, 98%, 89%)`,
-      dijon: `hsl(${dijon}, 100%, 50%)`,
-      obsidian: `hsl(${obsidian}, 75%, 9%)`,
-      glasgow: `hsl(${obsidian}, 95%, 16%)`,
-      paper: `hsl(${paper}, 100%, 99%)`,
-      parchment: `hsl(${paper}, 88%, 95%)`,
+const { styled, createTheme, css, globalCss, keyframes, getCssText, theme } =
+  createStitches({
+    theme: {
+      radii: {
+        "2": "2px",
+      },
+      borderWidths: {
+        linkFocus: "3px",
+      },
+      colors: {
+        // Base tones
+        angrypeach: `hsl(${angrypeach}, 100%, 80%)`,
+        candyfloss: `hsl(${candyfloss}, 98%, 89%)`,
+        dijon: `hsl(${dijon}, 100%, 50%)`,
+        obsidian: `hsl(${obsidian}, 75%, 9%)`,
+        glasgow: `hsl(${obsidian}, 95%, 16%)`,
+        paper: `hsl(${paper}, 100%, 99%)`,
+        parchment: `hsl(${paper}, 88%, 95%)`,
 
-      // Shades
-      // TODO: 'parchment' is a shade of 'paper', and 'glasgow' is a shade
-      //       of 'obsidian'. Come up with a more consistent approach to
-      //       defining shades.
-      candyflossLite: `hsl(${candyfloss}, 100%, 88%)`,
+        // Shades
+        // TODO: 'parchment' is a shade of 'paper', and 'glasgow' is a shade
+        //       of 'obsidian'. Come up with a more consistent approach to
+        //       defining shades.
+        candyflossLite: `hsl(${candyfloss}, 100%, 88%)`,
 
-      // Semantic aliases
-      accent: "$candyfloss",
-      bg: "$paper",
-      altBg: "$parchment",
-      contrastBg: "$obsidian",
-      contrastText: "$paper",
-      contrast: "$glasgow",
+        // Semantic aliases
+        accent: "$candyfloss",
+        bg: "$paper",
+        altBg: "$parchment",
+        contrastBg: "$obsidian",
+        contrastText: "$paper",
+        contrast: "$glasgow",
 
-      // Components
-      blockquoteBg: `$altBg`,
-      bodyText: `hsl(${obsidian}, 85%, 17%)`,
-      codeComment: `hsl(${obsidian}, 20%, 60%)`,
-      codeError: "$angrypeach",
-      codeKeyword: `hsl(${candyfloss}, 100%, 85%)`,
-      codeInlineBg: `$altBg`,
-      codeInlineBorder: `hsl(${paper}, 84%, 91%)`,
-      codeLiteral: `hsl(${paper}, 90%, 70%)`,
-      codeName: `hsl(${paper}, 100%, 84%)`,
-      codeOperator: `hsl(${obsidian}, 50%, 80%)`,
-      codePlain: "$contrastText",
-      codePunctuation: `hsl(${obsidian}, 18%, 55%)`,
-      dateText: `hsl(${obsidian}, 30%, 46%)`,
-      headerBorder: `hsl(${obsidian}, 85%, 20%)`,
-      headerText: `hsl(${obsidian}, 85%, 17%)`,
-      linkBorder: `hsl(${candyfloss}, 92%, 92%)`,
-      linkBorderHover: "$candyfloss",
-      linkText: "$bodyText",
-      linkHoverText: "inherit",
-      linkFocusBg: "$dijon",
-      linkFocusBorder: "$obsidian",
-      linkFocusText: "inherit",
+        // Components
+        blockquoteBg: `$altBg`,
+        bodyText: `hsl(${obsidian}, 85%, 17%)`,
+        codeComment: `hsl(${obsidian}, 20%, 60%)`,
+        codeError: "$angrypeach",
+        codeKeyword: `hsl(${candyfloss}, 100%, 85%)`,
+        codeInlineBg: `$altBg`,
+        codeInlineBorder: `hsl(${paper}, 84%, 91%)`,
+        codeLiteral: `hsl(${paper}, 90%, 70%)`,
+        codeName: `hsl(${paper}, 100%, 84%)`,
+        codeOperator: `hsl(${obsidian}, 50%, 80%)`,
+        codePlain: "$contrastText",
+        codePunctuation: `hsl(${obsidian}, 18%, 55%)`,
+        dateText: `hsl(${obsidian}, 30%, 46%)`,
+        headerBorder: `hsl(${obsidian}, 85%, 20%)`,
+        headerText: `hsl(${obsidian}, 85%, 17%)`,
+        linkBorder: `hsl(${candyfloss}, 92%, 92%)`,
+        linkBorderHover: "$candyfloss",
+        linkText: "$bodyText",
+        linkHoverText: "inherit",
+        linkFocusBg: "$dijon",
+        linkFocusBorder: "$obsidian",
+        linkFocusText: "inherit",
+      },
+      fonts: {
+        body: typography.options.bodyFontFamily.join(","),
+        code: 'Consolas, Monaco, "Andale Mono", "Ubuntu Mono", monospace',
+        header: typography.options.headerFontFamily.join(","),
+      },
+      fontWeights: {
+        regular: "400",
+        bold: "700",
+      },
+      sizes: {
+        maxWidthBestForText: "37rem",
+        maxWidthRegular: "56rem",
+      },
+      space,
     },
-    fonts: {
-      body: typography.options.bodyFontFamily.join(","),
-      code: 'Consolas, Monaco, "Andale Mono", "Ubuntu Mono", monospace',
-      header: typography.options.headerFontFamily.join(","),
+    media: {
+      bp1: "(min-width: 900px)",
     },
-    fontWeights: {
-      regular: "400",
-      bold: "700",
-    },
-    sizes: {
-      maxWidthBestForText: "37rem",
-      maxWidthRegular: "56rem",
-    },
-    space,
-  },
-  media: {
-    bp1: "(min-width: 900px)",
-  },
-  utils: {
-    marginX(value: PropertyValue<"margin">) {
-      return {
-        marginLeft: value,
-        marginRight: value,
-      };
-    },
+    utils: {
+      marginX(value: PropertyValue<"margin">) {
+        return {
+          marginLeft: value,
+          marginRight: value,
+        };
+      },
 
-    marginY(value: PropertyValue<"margin">) {
-      return {
-        marginBottom: value,
-        marginTop: value,
-      };
-    },
+      marginY(value: PropertyValue<"margin">) {
+        return {
+          marginBottom: value,
+          marginTop: value,
+        };
+      },
 
-    paddingX(value: PropertyValue<"padding">) {
-      return {
-        paddingLeft: value,
-        paddingRight: value,
-      };
-    },
+      paddingX(value: PropertyValue<"padding">) {
+        return {
+          paddingLeft: value,
+          paddingRight: value,
+        };
+      },
 
-    paddingY(value: PropertyValue<"padding">) {
-      return {
-        paddingBottom: value,
-        paddingTop: value,
-      };
-    },
+      paddingY(value: PropertyValue<"padding">) {
+        return {
+          paddingBottom: value,
+          paddingTop: value,
+        };
+      },
 
-    size(value: PropertyValue<"width">) {
-      return {
-        height: value,
-        width: value,
-      };
+      size(value: PropertyValue<"width">) {
+        return {
+          height: value,
+          width: value,
+        };
+      },
     },
-  },
-});
+  });
 
 const darkSection = createTheme({
   colors: {
